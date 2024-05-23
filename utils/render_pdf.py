4f766bcd6edbc2a992f07cfaefddd1604b63ef7e
Sonya <sonya.castro.gomez@gmail.com>
2024-05-22 22:46:23 -0500

    Fix: pdf render (again (again))

--- utils/render_pdf.py
+++ utils/render_pdf.py
@@ -2,18 +2,12 @@
 import base64
 import streamlit as st 
 
-def renderPDF(file):
-    # Opening file from file path
-    with open(file, "rb") as f:
+def renderPDF(file_path):
+    # Embedding PDF in HTML
+    with open(file_path, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
-    # Embedding PDF in HTML
-    pdf_display =  f"""<embed
-    class="pdfobject"
-    type="application/pdf"
-    title="Embedded PDF"
-    src="data:application/pdf;base64,{base64_pdf}"
-    style="overflow: auto; width: 100%; height: 800px;">"""
-
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    
     # Displaying File
     st.markdown(pdf_display, unsafe_allow_html=True)
