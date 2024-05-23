9dd116468b75e859af5d431d52eaa38502324557
Sonya <sonya.castro.gomez@gmail.com>
2024-05-22 22:30:42 -0500

    Fix: pdf render (again)

--- utils/render_pdf.py
+++ utils/render_pdf.py
@@ -8,7 +8,12 @@ def renderPDF(file):
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
     # Embedding PDF in HTML
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    pdf_display =  f"""<embed
+    class="pdfobject"
+    type="application/pdf"
+    title="Embedded PDF"
+    src="data:application/pdf;base64,{base64_pdf}"
+    style="overflow: auto; width: 100%; height: 800px;">"""
 
     # Displaying File
     st.markdown(pdf_display, unsafe_allow_html=True)
